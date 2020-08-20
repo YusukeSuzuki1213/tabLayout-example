50cbe8285a5d8f3274097a0b766f8077920806b2
YusukeSuzuki1213 <cs16047@s.inf.shizuoka.ac.jp>
2020-08-20 23:30:38 +0900

    updated collectionDemoFragment

--- app/src/main/java/com/example/tablayout_example/home/presentation/screen/CollectionDemoFragment.kt
+++ app/src/main/java/com/example/tablayout_example/home/presentation/screen/CollectionDemoFragment.kt
@@ -4,25 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.viewpager.widget.ViewPager
 import com.example.tablayout_example.R
+import com.example.tablayout_example.databinding.FragmentDemoObjectBinding
 import com.example.tablayout_example.home.presentation.screen.adapter.DemoCollectionPagerAdapter
 
 class CollectionDemoFragment : Fragment() {
 
     private lateinit var demoCollectionPagerAdapter: DemoCollectionPagerAdapter
-    private lateinit var viewPager: ViewPager
+    private lateinit var binding : FragmentDemoObjectBinding
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_collection_demo, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_collection_demo, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         demoCollectionPagerAdapter = DemoCollectionPagerAdapter(childFragmentManager)
-        viewPager = view.findViewById(R.id.pager)
-        viewPager.adapter = demoCollectionPagerAdapter
+
+        binding.run {
+            pager.adapter = demoCollectionPagerAdapter
+            tabLayout.setupWithViewPager(this.pager)
+        }
     }
 }
